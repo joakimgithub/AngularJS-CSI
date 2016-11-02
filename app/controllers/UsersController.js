065cc9be555b52eed7887aa5d7193c232ac8be71
joakimgithub <joakim.gunnarsson@sogeti.com>
2016-11-02 10:56:41 +0100

    .

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -1,10 +1,20 @@
 'use strict';
-app.controller("usersController", ['$scope', 'usersService', function ($scope, usersService) {
+app.controller("usersController", ['$scope', 'usersService', 'intCustomerService',
+                                   function ($scope, usersService, intCustomerService) {
 
     $scope.pageTitle = 'Users';
 
     usersService.getUserList().then(function (results) {
         $scope.users = results.data;
+    }, function (error) {
+        //alert(error.data.message);
+    });
+
+    // *******************************************
+    // Get all customers for a list of value
+    // *******************************************
+    intCustomerService.getCustomerList().then(function (results) {
+        $scope.lovCustomers = results.data;
     }, function (error) {
         //alert(error.data.message);
     });
