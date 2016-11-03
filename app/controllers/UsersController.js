a8391d79bbc21299b9f85a92285fba7327cba075
Praek44 <janne@jalin.se>
2016-11-03 09:05:39 +0100

    Rättat refresh

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -51,7 +51,7 @@ app.controller("usersController", ['$scope', 'usersService', 'intCustomerService
     };
 
     $scope.refresh = function(){
-        intCustomerService.getCustomerList().then(function (results) {
+        intCustomerService.getUserList().then(function (results) {
             $scope.users = results.data;
         }, function (error) {
             alert(error.data.message);
