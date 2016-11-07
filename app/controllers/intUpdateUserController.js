6c5e087a995b1f59c45bdf4fc7c3acb6621645fe
joakimgithub <joakim.gunnarsson@sogeti.com>
2016-11-07 13:49:04 +0100

    .

--- app/controllers/intUpdateUserController.js
+++ app/controllers/intUpdateUserController.js
@@ -2,14 +2,8 @@
 app.controller('intUpdateUserController', ['$scope', '$routeParams', '$location', '$timeout', 'authService', 'intCustomerService', function ($scope, $routeParams, $location, $timeout, authService, intCustomerService) {
 
     $scope.user = angular.fromJson($routeParams.theUser);
-
-//    $scope.user = $routeParams.theUser;
-//    $scope.theUserInt = $routeParams.theUser;
     $scope.savedSuccessfully = false;
     $scope.message = "";
-
-    debugger
-
 
 
     // *******************************************
@@ -18,32 +12,32 @@ app.controller('intUpdateUserController', ['$scope', '$routeParams', '$location'
     intCustomerService.getCustomerList().then(function (results) {
         $scope.lovCustomers = results.data;
     }, function (error) {
-        //alert(error.data.message);
+        alert(error.data.message);
     });
 
-//    $scope.addUser = function (user) {
-//        authService.updateUser($scope.user).then(function (response) {
-//
-//            $scope.savedSuccessfully = true;
-//            $scope.message = "User has been updated successfully";
-//            startTimer();
-//
-//        },function (response) {
-//             var errors = [];
-//             for (var key in response.data.ModelState) {
-//                 for (var i = 0; i < response.data.ModelState[key].length; i++) {
-//                     errors.push(response.data.ModelState[key][i]);
-//                 }
-//             }
-//             $scope.message = "Failed to update user due to:" + errors.join(' ');
-//         });
-//    };
-//
-//    var startTimer = function () {
-//        var timer = $timeout(function () {
-//            $timeout.cancel(timer);
-//            $location.path('/intUser');
-//        }, 2000);
-//    }
+    $scope.addUser = function (user) {
+        authService.updateUser($scope.user).then(function (response) {
+
+            $scope.savedSuccessfully = true;
+            $scope.message = "User has been updated successfully";
+            startTimer();
+
+        },function (response) {
+             var errors = [];
+             for (var key in response.data.ModelState) {
+                 for (var i = 0; i < response.data.ModelState[key].length; i++) {
+                     errors.push(response.data.ModelState[key][i]);
+                 }
+             }
+             $scope.message = "Failed to update user due to:" + errors.join(' ');
+         });
+    };
+
+    var startTimer = function () {
+        var timer = $timeout(function () {
+            $timeout.cancel(timer);
+            $location.path('/intUser');
+        }, 2000);
+    }
 
 }]);
